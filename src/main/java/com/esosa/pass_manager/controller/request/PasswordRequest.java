7ee55181d0997a501e34728b4b4c46a69fec077a
EzeSosa <ezequielsosa423@gmail.com>
2024-07-19 14:02:36 -0300

    ref: password name validation

--- src/main/java/com/esosa/pass_manager/controller/request/PasswordRequest.java
+++ src/main/java/com/esosa/pass_manager/controller/request/PasswordRequest.java
@@ -5,6 +5,6 @@ import jakarta.validation.constraints.Size;
 
 public record PasswordRequest(
         @NotNull(message = "Name must not be null")
-        @Size(max = 30, message = "Name size must be less than 30 characters")
+        @Size(min = 2, max = 30, message = "Name size must be between 2 and 30 characters")
         String name
 ) {}
