88e5b126e76e614d22aef1636e609a211cf6d724
EzeSosa <ezequielsosa423@gmail.com>
2024-07-20 21:03:20 -0300

    ref: remove cors annotation

--- src/main/java/com/esosa/pass_manager/controllers/UserController.java
+++ src/main/java/com/esosa/pass_manager/controllers/UserController.java
@@ -4,7 +4,6 @@ import com.esosa.pass_manager.controllers.response.PasswordResponse;
 import com.esosa.pass_manager.services.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +15,6 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/users")
-@CrossOrigin("http://localhost:3000")
 @Validated
 public class UserController {
     private final UserService userService;
